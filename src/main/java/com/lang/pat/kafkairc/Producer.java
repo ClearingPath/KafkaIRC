21df19d222b0312d72f95bd79141b461cb36831d
ClearingPath <13512093@std.stei.itb.ac.id>
2015-10-19 18:26:54 +0700

    implemented producer

--- src/main/java/com/lang/pat/kafkairc/Producer.java
+++ src/main/java/com/lang/pat/kafkairc/Producer.java
@@ -5,13 +5,28 @@
  */
 package com.lang.pat.kafkairc;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 /**
  *
  * @author ClearingPath
  */
 public class Producer {
+    private Properties props;            
+    private KafkaProducer<String,String> producer;
+    private String key = "lang.pat.key";
+    
+    
     public Producer(){
-        
+        props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        producer = new KafkaProducer<String,String>(props);
     }
     
     public void send(String Message){
@@ -21,6 +36,14 @@ public class Producer {
     }
     
     public void send(String Message, String Channel){
-        
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<String,String>(Channel, key, Message);
+        producer.send(producerRecord);
     }
+    
+    @Override
+    protected void finalize() throws Throwable{
+        producer.close();
+        super.finalize();
+    }
+    
 }
